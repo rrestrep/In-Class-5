8fe13707ca3f36a4b10f7ea84204fdc194646b45
rrestrep <46639752+rrestrep@users.noreply.github.com>
2019-02-26 21:21:01 -0500

    Add files via upload

---
+++ Create dbo.INdex_1.sql
@@ -0,0 +1,18 @@
+﻿USE [Project_5_Class]
+GO
+
+/****** Object: Table [dbo].[INdex] Script Date: 2/26/2019 9:18:45 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[INdex] (
+    [Stock_ID]       VARCHAR (50) NOT NULL,
+    [Stock_Price]    VARCHAR (50) NULL,
+    [Stock_Dividend] VARCHAR (50) NULL,
+    [Stock_Sell_oof] VARCHAR (50) NULL
+);
+
+
---
+++ Create dbo.Table_1.sql
@@ -0,0 +1,25 @@
+﻿USE [Project_5_Class]
+GO
+
+/****** Object: Table [dbo].[Table] Script Date: 2/26/2019 9:19:03 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Table] (
+    [Id]                        INT          NOT NULL,
+    [Company Name]              VARCHAR (50) NULL,
+    [Company_Address]           VARCHAR (50) NULL,
+    [Company_Phone_Number]      VARCHAR (50) NULL,
+    [Company_Owner]             VARCHAR (50) NULL,
+    [Company_Web_site]          VARCHAR (50) NULL,
+    [Stock_ID]                  VARCHAR (50) NULL,
+    [Company_Headquarther_City] VARCHAR (50) NULL,
+    [Company_Facebook]          VARCHAR (50) NULL,
+    [Company_Twiter]            VARCHAR (50) NULL,
+    [Company_Tax_ID]            VARCHAR (50) NULL
+);
+
+
