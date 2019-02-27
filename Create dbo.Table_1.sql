15fff3672cb12f9a334c0ffb6ce796d07a50643b
rrestrep <46639752+rrestrep@users.noreply.github.com>
2019-02-26 21:26:09 -0500

    Add files via upload

--- Create dbo.Table_1.sql
+++ Create dbo.Table_1.sql
@@ -8,6 +8,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
+
 CREATE TABLE [dbo].[Table] (
     [Id]                        INT          NOT NULL,
     [Company Name]              VARCHAR (50) NULL,
@@ -15,11 +16,14 @@ CREATE TABLE [dbo].[Table] (
     [Company_Phone_Number]      VARCHAR (50) NULL,
     [Company_Owner]             VARCHAR (50) NULL,
     [Company_Web_site]          VARCHAR (50) NULL,
-    [Stock_ID]                  VARCHAR (50) NULL,
+    [Stock_ID]          VARCHAR (50) NULL,
     [Company_Headquarther_City] VARCHAR (50) NULL,
     [Company_Facebook]          VARCHAR (50) NULL,
     [Company_Twiter]            VARCHAR (50) NULL,
-    [Company_Tax_ID]            VARCHAR (50) NULL
+    [Company_Tax_ID]            VARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC), 
+    CONSTRAINT [FK_Table_ToTable] FOREIGN KEY (Stock_ID) REFERENCES [INdex]([Stock_ID])
 );
+
 
 
